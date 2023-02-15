b8f8b97fa82f0e00398db1a47f9249cef15e6dd5
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-14 23:07:34 -0800

    updated users controller
    added todos, stubbed out routes under comments
    
    Co-Authored-By: Jagbir Mangat <105836617+jbmangat@users.noreply.github.com>
    Co-Authored-By: Joshua Lopez <105695636+Mediczilla@users.noreply.github.com>
    Co-Authored-By: Jamaine Haywood <105879061+jhaywood86@users.noreply.github.com>
    Co-Authored-By: Martiz27 <105825771+Martiz27@users.noreply.github.com>

---
+++ backend/controllers/users.js
@@ -0,0 +1,11 @@
+const router = require('express').Router()
+
+// TODO: POST USERS
+// router.post('/', async (req, res) => {
+
+// })
+
+// TODO: GET USERS
+// router.get('/', async (req, res) => {
+
+// })
