6897afeb579301187c3a2cbacd8bcb8976c69453
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-16 18:46:29 -0800

    added imports for components in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
+import WelcomePage from './WelcomePage'
+import StickyFooter from './StickyFooter'
+import Error404 from './Error404';
+import RecipeIndex from './recipes/RecipeIndex'
+import CurrentRecipe from './recipes/CurrentRecipe'
+import NewRecipeForm from './recipes/NewRecipeForm'
+import EditRecipeForm from './recipes/EditRecipeForm'
+import SignUpForm from './users/SignUpForm'
+import LoginForm from './users/LoginForm'
+import CurrentUserProvider from './contexts/CurrentUser'
+
 function App() {
   return (
+    // Starter React Code
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
