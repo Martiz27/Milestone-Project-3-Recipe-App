b4e014568fbd5a4e17d45773351878e6e80fe4b1
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-16 19:54:02 -0800

    utilitized react-bootstrap to add bottom fixed navbar with navbar brand

--- frontend/src/Error404.js
+++ frontend/src/Error404.js
@@ -1,5 +1,10 @@
 // error 404 page
 function Error404() {
+    <div class="error">
+        <img src="www.placeholder.com" alt="Error Page"/>
+        <h1>Oops! Looks like the page you were looking for was removed or is temporary not available</h1>
+
+    </div>
 
 }
 
--- frontend/src/StickyFooter.js
+++ frontend/src/StickyFooter.js
@@ -1,9 +1,48 @@
-// sticky footer navigation component
+import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
-// imports
-
+// TODO: Make nav horizontally aligned and positioned on the bottom
 function StickyFooter() {
-
+    return (
+        <Navbar fixed='bottom' expand='md' fill className='justify-content-center'>
+            <Container>
+                <Navbar.Brand href=''>FEAST MODE</Navbar.Brand>
+                <Nav variant='pills'>
+                    <NavDropdown align='end' drop='up' title='Categories' id='categories-dropdown'>
+                        <NavDropdown.Item>
+                            Breakfast
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item>
+                            Lunch
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item>
+                            Dinner
+                        </NavDropdown.Item>
+                        <NavDropdown.Divider />
+                        <NavDropdown.Item>
+                            Dessert
+                        </NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Item>
+                        <Nav.Link href=''>
+                            Favorites
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href=''>
+                            Add New Recipe
+                        </Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                        <Nav.Link href=''>
+                            User
+                        </Nav.Link>
+                    </Nav.Item>
+                </Nav>
+            </Container>
+        </Navbar>
+    )
 }
 
 export default StickyFooter;
