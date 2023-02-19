f0fe9814be226972ed3bb278ad686816fb7f8c41
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 15:35:58 -0800

    updated modules and express settings

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require("express")
-const PORT = process.env.PORT
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const path = require('path')
 const app = express()
+const PORT = process.env.PORT
 
 // Express Settings
+app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 // Controllers and Routes
 app.use('/recipes', require('./controllers/recipes.js'))
