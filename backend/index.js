69d65fc6f28d1fda6538302fb78f5ab72d6a045a
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 14:17:29 -0800

    removed unneccessary comments

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,16 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require("express")
-// const mongoose = require('mongoose')
 const PORT = process.env.PORT
 const app = express()
 
-// mongoose.connect(
-//     process.env.MONGO_URI, 
-//     { useNewUrlParser: true },
-//     () => { console.log('connected to mongo') }
-// )
+// Express Settings
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 // Controllers and Routes
 app.use('/recipes', require('./controllers/recipes.js'))
 app.use('/users', require('./controllers/users.js'))
-// app.get('/', (req, res) => {
-//     res.send('oops')
-// })
 
 // Listen for Connections
 app.listen(PORT, () => {
