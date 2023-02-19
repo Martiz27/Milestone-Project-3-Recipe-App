24ad96bc4ca336e39fcedb9fb00776e1f7f92a22
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 14:00:48 -0800

    added app.use for controllers and routes

--- backend/index.js
+++ backend/index.js
@@ -1,20 +1,24 @@
-const express = require("express")
-const mongoose = require('mongoose')
-
+// Modules and Globals
 require('dotenv').config()
+const express = require("express")
+// const mongoose = require('mongoose')
 const PORT = process.env.PORT
-
 const app = express()
 
-mongoose.connect(
-    process.env.MONGO_URI, { useNewUrlParser: true },
-    () => { console.log('connected to mongo') }
-)
+// mongoose.connect(
+//     process.env.MONGO_URI, 
+//     { useNewUrlParser: true },
+//     () => { console.log('connected to mongo') }
+// )
 
-app.get('/', (req, res) => {
-    res.send('oops')
-})
+// Controllers and Routes
+app.use('/recipes', require('./controllers/recipes.js'))
+app.use('/users', require('./controllers/users.js'))
+// app.get('/', (req, res) => {
+//     res.send('oops')
+// })
 
+// Listen for Connections
 app.listen(PORT, () => {
-    console.log(typeof MONGO_URI)
+    console.log(`listening on ${PORT}`)
 })
