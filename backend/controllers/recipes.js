050791984828d15ba2a18b2aab29de1ad7827111
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 15:35:00 -0800

    updated recipe index route

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,43 +1,48 @@
 const router = require('express').Router()
+const db = require('../models')
+
+const { Recipe } = db
 
 // TODO: RECIPE INDEX
 router.get('/', async (req, res) => {
-    res.send('Recipe index')
+    const recipes = await Recipe.findAll()
+    console.log(recipes)
+    res.json(recipes)
 })
 
 // TODO: NEW FORM
 router.get('/new', async (req, res) => {
-    res.send('New recipe')
+    res.json({Response: 'New recipe'})
 })
 
 // TODO: POST NEW
 router.post('/', async (req, res) => {
-    res.send('Successfully created recipe')
+    res.json({Response: 'Successfully created recipe'})
 })
 
 // TODO: EDIT FORM
 router.get('/:recipeId/edit', async (req, res) => {
-    res.send('Edit recipe form')
+    res.json({Response: 'Edit recipe form'})
 })
 
 // TODO: PUT EDIT
 router.put('/:recipeId', async (req, res) => {
-    res.send('Successfully updated recipe')
+    res.json({Response: 'Successfully updated recipe'})
 })
 
 // TODO: SHOW BY CATEGORY
 router.get('/:categoryId', async (req, res) => {
-    res.send('Show recipes in this category')
+    res.json({Response: 'Show recipes in this category'})
 })
 
 // TODO: SHOW
 router.get('/:recipeId', async (req, res) => {
-    res.send('Show current recipe')
+    res.json({Response : 'Show current recipe'})
 })
 
 // TODO: DELETE
 router.delete('/:recipeId', async (req, res) => {
-    res.send('Successfully deleted recipe')
+    res.json({Response: 'Successfully deleted recipe'})
 })
 
 module.exports = router;
