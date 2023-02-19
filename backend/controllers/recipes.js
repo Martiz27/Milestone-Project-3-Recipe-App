a15e040b1d5268bd773fbb94c981f9ca254c453c
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 14:05:15 -0800

    stubbed out recipes controller with test responses

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,44 +1,43 @@
 const router = require('express').Router()
 
-
-// TODO: INDEX
-// router.get('/', async (req, res) => {
-
-// })
+// TODO: RECIPE INDEX
+router.get('/', async (req, res) => {
+    res.send('Recipe index')
+})
 
 // TODO: NEW FORM
-// router.get('/new', async (req, res) => {
-
-// })
+router.get('/new', async (req, res) => {
+    res.send('New recipe')
+})
 
 // TODO: POST NEW
-// router.post('/', async (req, res) => {
-
-// })
+router.post('/', async (req, res) => {
+    res.send('Successfully created recipe')
+})
 
 // TODO: EDIT FORM
-// router.get('/:recipeId/edit', async (req, res) => {
-
-// })
+router.get('/:recipeId/edit', async (req, res) => {
+    res.send('Edit recipe form')
+})
 
 // TODO: PUT EDIT
-// router.put('/:recipeId', async (req, res) => {
-
-// })
+router.put('/:recipeId', async (req, res) => {
+    res.send('Successfully updated recipe')
+})
 
 // TODO: SHOW BY CATEGORY
-// router.get('/:categoryId', async (req, res) => {
-
-// })
+router.get('/:categoryId', async (req, res) => {
+    res.send('Show recipes in this category')
+})
 
 // TODO: SHOW
-// router.get('/:recipeId', async (req, res) => {
-
-// })
+router.get('/:recipeId', async (req, res) => {
+    res.send('Show current recipe')
+})
 
 // TODO: DELETE
-// router.delete('/:recipeId', async (req, res) => {
+router.delete('/:recipeId', async (req, res) => {
+    res.send('Successfully deleted recipe')
+})
 
-// })
-
-module.exports = router
+module.exports = router;
