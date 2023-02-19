b8e80cbffa5e09d585e7fdd1aca12d21275ea1de
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 12:06:18 -0800

    updated backend index.js with mongoose connection

--- backend/index.js
+++ backend/index.js
@@ -6,14 +6,15 @@ const PORT = process.env.PORT
 
 const app = express()
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () => {
-    console.log('connected to mongo')
-}) 
+mongoose.connect(
+    process.env.MONGO_URI, { useNewUrlParser: true },
+    () => { console.log('connected to mongo') }
+)
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send('oops')
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`)
+    console.log(typeof MONGO_URI)
 })
