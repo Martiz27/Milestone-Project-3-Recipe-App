d06a8f17f9a7862824c1f4a602a1ad0d284dffa4
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 15:35:16 -0800

    updated seeders

--- backend/seeders/testData.js
+++ backend/seeders/testData.js
@@ -9,7 +9,7 @@ Recipe.create ([
         snack: false,
         favorite: false,
         ingredients: 'Ingredient 1. Ingredient 2',
-        directions: 'Direction 1. Direction 2.',
+        directions: 'Direction 1. Direction 2',
         image: 'https://images.unsplash.com/photo-1576186726580-a816e8b12896?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80' //Photo by No Revisions on Unsplash
     }
 ])
