d4dae7000ff6ed2239c89863b9ae8afa3bd91e3f
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-19 14:01:22 -0800

    cleaned up formatting

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,7 +1,12 @@
-const mongoose = require('mongoose')
 require('dotenv').config()
+const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, 
-    useUnifiedTopology: true})
+mongoose.connect(
+    process.env.MONGO_URI,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+)
 
 module.exports.Recipe = require('./recipe.js')
