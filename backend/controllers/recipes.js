751373c84e0cc96d7af1b67a4f227f92894df7f4
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-20 14:26:15 -0800

    updated show route

--- backend/controllers/recipes.js
+++ backend/controllers/recipes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const db = require('../models')
+const mongoose = require('mongoose')
 
 const { Recipe } = db
 
@@ -11,37 +12,47 @@ router.get('/', async (req, res) => {
 
 // TODO: NEW FORM
 router.get('/new', async (req, res) => {
-    res.json({Response: 'New recipe'})
+    res.json({ Response: 'New recipe' })
 })
 
 // TODO: POST NEW
 router.post('/', async (req, res) => {
-    res.json({Response: 'Successfully created recipe'})
+    res.json({ Response: 'Successfully created recipe' })
 })
 
 // TODO: EDIT FORM
 router.get('/:recipeId/edit', async (req, res) => {
-    res.json({Response: 'Edit recipe form'})
+    res.json({ Response: 'Edit recipe form' })
 })
 
 // TODO: PUT EDIT
 router.put('/:recipeId', async (req, res) => {
-    res.json({Response: 'Successfully updated recipe'})
-})
-
-// TODO: SHOW BY CATEGORY
-router.get('/:categoryId', async (req, res) => {
-    res.json({Response: 'Show recipes in this category'})
+    res.json({ Response: 'Successfully updated recipe' })
 })
 
 // TODO: SHOW
 router.get('/:recipeId', async (req, res) => {
-    res.json({Response : 'Show current recipe'})
+    let recipeId = req.params.recipeId
+    // FIXIT: id error check
+    // if (!mongoose.Types.ObjectId.isValid(req.param.recipeId)) {
+    //     return res.status(404).json({message: 'Recipe id is not valid'})
+    // }
+    const recipe = await Recipe.findById(req.params.recipeId)
+    if (!recipe) {
+        return res.status(404).json({message: 'Recipe does not exist.'})
+    }
+    console.log(recipe)
+    res.status(200).json(recipe)
+})
+
+// TODO: SHOW BY CATEGORY
+router.get('/:categoryId', async (req, res) => {
+    res.json({ Response: 'Show recipes in this category' })
 })
 
 // TODO: DELETE
 router.delete('/:recipeId', async (req, res) => {
-    res.json({Response: 'Successfully deleted recipe'})
+    res.json({ Response: 'Successfully deleted recipe' })
 })
 
 module.exports = router;
