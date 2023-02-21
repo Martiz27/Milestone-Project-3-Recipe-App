e48711d683e79ee25ed9928dd9e767d0c92f744b
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-20 17:36:17 -0800

    updated NavBar.Brand onClick to navigate back to recipes index

--- frontend/src/StickyFooter.js
+++ frontend/src/StickyFooter.js
@@ -1,50 +1,53 @@
+import { useNavigate } from 'react-router-dom'
 import { Container, Navbar, Nav, NavDropdown } from 'react-bootstrap'
 
 // TODO: Make nav horizontally aligned and positioned on the bottom
 function StickyFooter() {
+    const navigate = useNavigate()
+
     return (
         <Navbar collapseOnSelect expand='md' bg='light' fixed='bottom' fill='true'>
-                <Container className=''>
-                    <Navbar.Brand href=''>FEAST MODE</Navbar.Brand>
-                    <Navbar.Toggle />
-                    <Navbar.Collapse id='responsive-navbar-nav' >
-                        <Nav variant='pills' className='justify-content-around' style={{flex:1}}>
-                            <NavDropdown align='end' drop='up' title='Categories' id='categories-dropdown'>
-                                <NavDropdown.Item>
-                                    Breakfast
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item>
-                                    Lunch
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item>
-                                    Dinner
-                                </NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item>
-                                    Dessert
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                            <Nav.Item>
-                                <Nav.Link href=''>
-                                    Favorites
-                                </Nav.Link>
-                            </Nav.Item>
-                            <Nav.Item>
-                                <Nav.Link href=''>
-                                    Add New Recipe
-                                </Nav.Link>
-                            </Nav.Item>
-                            {/* TODO: User Context */}
-                            <NavDropdown align='end' drop='up' title='User' id='user-dropdown'>
-                                <NavDropdown.Item>
-                                    Logout
-                                </NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
+            <Container className=''>
+                <Navbar.Brand onClick={() => navigate('/recipes')}>FEAST MODE</Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse id='responsive-navbar-nav' >
+                    <Nav variant='pills' className='justify-content-around' style={{ flex: 1 }}>
+                        <NavDropdown align='end' drop='up' title='Categories' id='categories-dropdown'>
+                            <NavDropdown.Item>
+                                Breakfast
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                Lunch
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                Dinner
+                            </NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>
+                                Dessert
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                        <Nav.Item>
+                            <Nav.Link href=''>
+                                Favorites
+                            </Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href=''>
+                                Add New Recipe
+                            </Nav.Link>
+                        </Nav.Item>
+                        {/* TODO: User Context */}
+                        <NavDropdown align='end' drop='up' title='User' id='user-dropdown'>
+                            <NavDropdown.Item>
+                                Logout
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
