b92bc302fdc53ba6014b531f2b6a3c0a17984b81
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-21 15:27:56 -0800

    updated Error404 component with react-bootstrap styling and added Go Back button using navigate

--- frontend/src/Error404.js
+++ frontend/src/Error404.js
@@ -1,16 +1,29 @@
-import { Route, Routes } from "react-router-dom"
+import { useNavigate } from 'react-router'
+import { Container, Card, Row, Button } from 'react-bootstrap'
 import WelcomePage from "./WelcomePage";
+import errorImg from './error404.jpg'
+import { BsArrowLeftShort } from 'react-icons/bs';
 
 function Error404() {
+    const navigate = useNavigate()
+
     return (
-        <div class="error">
-            <img src="../public/404img.png" alt="Error Page" />
-            <h1>404</h1>
-            <h1>Oops! Looks like the page you were looking for was removed or is temporary not available</h1>
-            <Routes>
-                <Route path="/" element={<WelcomePage />} />
-            </Routes>
-        </div>
+        <Container className='my-5 mx-auto pb-5'>
+            <Card className='w-75 mx-auto'>
+                <Card.Img src={errorImg} className='img-fluid rounded-0' />
+                <Card.ImgOverlay className='bg-dark bg-opacity-75 text-light text-center'>
+                    <Card.Title><h1 className='text-warning'>404</h1></Card.Title>
+                    <Card.Body>
+                        <Card.Text>
+                            <h3>Oops! Looks like the page you were looking for was removed or is temporary not available</h3>
+                        </Card.Text>
+                        <Row className='mb-3 position-absolute bottom-0'>
+                            <Button variant='light' size='sm' onClick={() => navigate(-1)}><BsArrowLeftShort /> Go Back </Button>
+                        </Row>
+                    </Card.Body>
+                </Card.ImgOverlay>
+            </Card>
+        </Container>
     )
 }
 
