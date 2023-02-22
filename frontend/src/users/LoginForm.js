2dfce55e89af7d550141bef8801956edb4d5cdca
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-21 18:28:38 -0800

    added login and signup forms and todos

--- frontend/src/users/LoginForm.js
+++ frontend/src/users/LoginForm.js
@@ -1,8 +1,84 @@
-// login user form
+import { useState, useContext } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Container, Form, FormLabel, FloatingLabel, Row, Col, Button, ButtonToolbar } from 'react-bootstrap'
+import { BsEgg, BsEggFill, BsEggFried } from 'react-icons/bs'
+import { CurrentUser } from '../contexts/CurrentUser'
 
-// imports
 function LoginForm() {
-    
+
+    const navigate = useNavigate()
+
+    const {setCurrentUser} = useContext(CurrentUser)
+
+    // TODO: SET USER SCHEMA FIRST 
+    // TODO: INITIALIZE USESTATE
+    const [userCred, setUserCred] = useState({
+
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        navigate('/recipes')
+    }
+
+    return (
+        <Container className='my-5 mx-auto pb-5'>
+            <h1 className='text-warning'>Login</h1>
+            <hr />
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>
+                    <h3 className='text-primary'>Enter Email and Password</h3>
+                </Form.Label>
+                <Row className='mb-3 g-3'>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floatingInput'
+                                label='Email'>
+                                <Form.Control
+                                    required
+                                    type='email'
+                                    placeholder='Email'
+                                    id='email'
+                                    name='email'
+                                    value={userCred.email}
+                                    onChange={e => setUserCred({ ...userCred, email: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floatingInput'
+                                label='Password'>
+                                <Form.Control
+                                    required
+                                    type='password'
+                                    placeholder='Password'
+                                    id='password'
+                                    name='password'
+                                    value={userCred.password}
+                                    onChange={e => setUserCred({ ...userCred, password: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <ButtonToolbar className='d-flex justify-content-end gap-3'>
+                    <Button variant='light' size='sm' type='submit' onClick={() => navigate('/signup')}>
+                        <BsEgg className='mb-1' /> Go to Sign Up Page
+                    </Button>
+                    <Button variant='danger' size='sm' onClick={() => navigate('/')}>
+                        <BsEggFill className='mb-1' /> Cancel
+                    </Button>
+                    <Button variant='warning' size='sm' type='submit'>
+                        <BsEggFried className='mb-1' /> Login
+                    </Button>
+                </ButtonToolbar>
+            </Form>
+        </Container>
+    )
 }
 
 export default LoginForm;
--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -1,8 +1,148 @@
-// sign up form to create a new user
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Container, Form, FormLabel, FloatingLabel, Row, Col, Button, ButtonToolbar } from 'react-bootstrap'
+import { BsEgg, BsEggFill, BsEggFried } from 'react-icons/bs'
 
-// imports
 function SignUpForm() {
 
+    const navigate = useNavigate()
+
+    // TODO: SET USER SCHEMA FIRST 
+    // TODO: INITIALIZE USESTATE
+    const [user, setUser] = useState({
+
+    })
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+
+        await fetch(`http://localhost:5000/users/`, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        })
+
+        navigate('/recipes')
+    }
+
+    return (
+        <Container className='my-5 mx-auto pb-5'>
+            <h1 className='text-warning'>Sign Up</h1>
+            <hr />
+            <Form onSubmit={handleSubmit}>
+                <Form.Label>
+                    <h3 className='text-primary'>Personal Information</h3>
+                </Form.Label>
+                <Row className='mb-3 g-3'>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floating-input'
+                                label='First Name'
+                            >
+                                <Form.Control
+                                    required
+                                    type='text'
+                                    placeholder='First Name'
+                                    id='firstName'
+                                    name='firstName'
+                                    value={user.firstName}
+                                    onChange={e => setUser({ ...user, firstName: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floating-input'
+                                label='Last Name'
+                            >
+                                <Form.Control
+                                    required
+                                    type='text'
+                                    placeholder='Last Name'
+                                    id='lastName'
+                                    name='lastName'
+                                    value={user.lastName}
+                                    onChange={e => setUser({ ...user, lastName: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                    <Form.Group>
+                        <FloatingLabel
+                            controlID='floatingInput'
+                            label='Email'>
+                            <Form.Control
+                                required
+                                type='email'
+                                placeholder='Email'
+                                id='email'
+                                name='email'
+                                value={user.email}
+                                onChange={e => setUser({ ...user, email: e.target.value })}
+                            />
+                        </FloatingLabel>
+                    </Form.Group>
+                </Row>
+                <hr />
+                {/* TODO: MATCHING PASSWORD INPUT CONFIRMATION */}
+                <FormLabel>
+                    <h3 className='text-primary'>Login Information</h3>
+                </FormLabel>
+                <Row className='mb-3 g-3'>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floatingInput'
+                                label='Password'>
+                                <Form.Control
+                                    required
+                                    type='password'
+                                    placeholder='Password'
+                                    id='password'
+                                    name='password'
+                                    value={user.password}
+                                    onChange={e => setUser({ ...user, password: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                controlID='floatingInput'
+                                label='Confirm Password'>
+                                <Form.Control
+                                    required
+                                    type='password'
+                                    placeholder='Confirm Password'
+                                    id='password'
+                                    name='password'
+                                    value={user.password}
+                                    onChange={e => setUser({ ...user, password: e.target.value })}
+                                />
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <ButtonToolbar className='d-flex justify-content-end gap-3'>
+                    <Button variant='light' size='sm' onClick={() => navigate('/login')}>
+                        <BsEggFried className='mb-1' /> Go to Login Page
+                    </Button>
+                    <Button variant='danger' size='sm' onClick={() => navigate('/')}>
+                        <BsEggFill className='mb-1' /> Cancel
+                    </Button>
+                    <Button variant='warning' size='sm' type='submit' >
+                        <BsEgg className='mb-1' /> Sign Up
+                    </Button>
+                </ButtonToolbar>
+            </Form>
+        </Container>
+    )
 }
 
 export default SignUpForm;
