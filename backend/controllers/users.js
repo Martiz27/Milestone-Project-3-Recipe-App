9f18842247a40135593434c6416240accab50285
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-21 18:27:26 -0800

    updated post and get user routes

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -4,12 +4,16 @@ const { User } = db
 
 // TODO: POST USERS
 router.post('/', async (req, res) => {
-    res.send("Update recipe index with user's recipes")
+    // res.send("Update recipe index with user's recipes")
+    const user = await User.create(req.body)
+    res.json(user)
 })
 
 // TODO: GET USERS
 router.get('/', async (req, res) => {
-    res.send("Getting user's information")
+    // res.send("Getting user's information")
+    const users = await User.find()
+    res.json(users)
 })
 
 module.exports = router
