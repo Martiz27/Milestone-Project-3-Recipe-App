3d92d3f61cf5c8e6db617e1949b7bcb2508ee46e
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-23 08:43:25 -0800

    updated StickyFooter component to display different navbars based on CurrentUser context

--- frontend/src/StickyFooter.js
+++ frontend/src/StickyFooter.js
@@ -1,64 +1,119 @@
+import { useContext } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Container, Navbar, Nav, Button, Form, InputGroup } from 'react-bootstrap'
 import { BsSearch, BsStarFill, BsPlus, BsEgg, BsEggFill } from 'react-icons/bs'
 import Cookies from 'universal-cookie'
+import { CurrentUser } from './contexts/CurrentUser';
 
 // TODO: Make nav horizontally aligned and positioned on the bottom
 function StickyFooter() {
+
     const navigate = useNavigate()
 
     const cookies = new Cookies()
 
     const token = cookies.get('TOKEN')
 
-    const logout = () => {
-        // destroy cookie
-        cookies.remove('TOKEN', { path: '/' })
-        navigate('/')
+    const { currentUser, setCurrentUser } = useContext(CurrentUser)
+
+    async function logout(e) {
+        try {
+            e.preventDefault()
+
+            // TODO: TO REMOVE USER FROM STATE AND DESTROY TOKENS
+            // let res = await fetch(`http://localhost:5000/user/logout`, {
+            //     method: 'DELETE',
+            //     headers: {
+            //         'Content-Type': 'application/json'
+            //     },
+            //     body: JSON.stringify(currentUser)
+            // })
+
+            console.log('Destroying token')
+            cookies.remove(token, { path: '/' })
+            setCurrentUser(null)
+            navigate('/')
+        } catch (err) {
+            console.log(err)
+        }
     }
 
-    return (
-        <Navbar collapseOnSelect expand='lg' bg='light' fixed='bottom' fill='true'>
-            <Container>
-                <Navbar.Brand onClick={() => navigate('/recipes')} className='text-danger'>
-                    FEAST MODE
-                </Navbar.Brand>
-                <Navbar.Toggle className='my-2' />
-                <Navbar.Collapse id='responsive-navbar-nav' >
+    let loginActions = (
+        <>
+            <Navbar bg='light' fixed='bottom' fill='true'>
+                <Container>
+                    <Navbar.Brand onClick={() => navigate('/')}>
+                        FEAST MODE
+                    </Navbar.Brand>
                     <Nav variant='pills' className='justify-content-around g-2' style={{ flex: 1 }}>
-                        <Nav.Item>
-                            <InputGroup className='d-flex p-1'>
-                                <InputGroup.Text id='search'><BsSearch /></InputGroup.Text>
-                                <Form.Control
-                                    placeholder='Search for Category'
-                                />
-                            </InputGroup>
-                        </Nav.Item>
                         <Nav.Item className='d-flex p-1'>
-                            <Button variant='light' size='sm' onClick={() => navigate('/')}>
-                                <BsStarFill className='mb-1' /> Favorites
+                            <Button variant='warning' size='sm' onClick={() => navigate('/info/features')}>
+                                <BsEgg className='mb-1' /> Features
                             </Button>
                         </Nav.Item>
                         <Nav.Item className='d-flex p-1'>
-                            <Button variant='light' size='sm' onClick={() => navigate('/recipes/new')}>
-                                <BsPlus className='mb-1' /> Add Recipe
-                            </Button>
-                        </Nav.Item>
-                        <Nav.Item className='d-flex p-1'>
-                            <Button variant='light' size='sm' onClick={() => navigate('/')}>
-                                <BsEgg className='mb-1' /> Help
-                            </Button>
-                        </Nav.Item>
-                        {/* TODO: Update Navigate Path For Logout Button */}
-                        <Nav.Item className='d-flex p-1'>
-                            <Button variant='danger' size='sm' type='submit' onClick={() => logout()}>
-                                <BsEggFill className='mb-1' /> Logout
+                            <Button variant='warning' size='sm' type='submit' onClick={() => navigate('/info/about')}>
+                                <BsEggFill className='mb-1' /> About
                             </Button>
                         </Nav.Item>
                     </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+                </Container>
+            </Navbar>
+        </>
+    )
+
+
+    if (currentUser) {
+        loginActions = (
+            <>
+                <Navbar collapseOnSelect expand='lg' bg='light' fixed='bottom' fill='true'>
+                    <Container>
+                        <Navbar.Brand onClick={() => navigate('/recipes')} className='text-danger'>
+                            FEAST MODE
+                        </Navbar.Brand>
+                        <Navbar.Toggle className='my-2' />
+                        <Navbar.Collapse id='responsive-navbar-nav' >
+                            <Nav variant='pills' className='justify-content-around g-2' style={{ flex: 1 }}>
+                                <Nav.Item>
+                                    <InputGroup className='d-flex p-1'>
+                                        <InputGroup.Text id='search'><BsSearch /></InputGroup.Text>
+                                        <Form.Control
+                                            placeholder='Search for Category'
+                                        />
+                                    </InputGroup>
+                                </Nav.Item>
+                                <Nav.Item className='d-flex p-1'>
+                                    <Button variant='light' size='sm' onClick={() => navigate('/recipes?favorites=true')}>
+                                        <BsStarFill className='mb-1' /> Favorites
+                                    </Button>
+                                </Nav.Item>
+                                <Nav.Item className='d-flex p-1'>
+                                    <Button variant='light' size='sm' onClick={() => navigate('/recipes/new')}>
+                                        <BsPlus className='mb-1' /> Add Recipe
+                                    </Button>
+                                </Nav.Item>
+                                <Nav.Item className='d-flex p-1'>
+                                    <Button variant='light' size='sm' onClick={() => navigate('/info/help')}>
+                                        <BsEgg className='mb-1' /> Help
+                                    </Button>
+                                </Nav.Item>
+                                <Nav.Item className='d-flex p-1'>
+                                    <Button variant='danger' size='sm' type='submit' onClick={(e) => logout(e)}>
+                                        <BsEggFill className='mb-1' /> Logout
+                                    </Button>
+                                </Nav.Item>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
+        )
+    }
+
+    return (
+        <>
+            {loginActions}
+        </>
     )
 }
 
