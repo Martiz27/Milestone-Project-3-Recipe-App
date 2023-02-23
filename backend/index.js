00802231b6f1feffe0f8b8194049c2eaee71ccd4
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-23 08:00:10 -0800

    removed path, added defineCurrentUser middleware

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,23 @@ require('dotenv').config()
 const express = require("express")
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const path = require('path')
 const app = express()
-const PORT = process.env.PORT
+const defineCurrentUser = require('./authorization/defineUser')
 const mongoose = require('mongoose')
 mongoose.set('strictQuery', true);
+const PORT = process.env.PORT
 
 // Express Settings
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(defineCurrentUser)
 
 // Controllers 
-app.use('/recipes', require('./controllers/recipes.js'))
-app.use('/users', require('./controllers/users.js'))
+app.use('/recipes', require('./controllers/recipes'))
+app.use('/user', require('./controllers/users'))
+app.use('/auth', require('./controllers/authenticate'))
 
 // Listen for Connections
 app.listen(PORT, () => {
