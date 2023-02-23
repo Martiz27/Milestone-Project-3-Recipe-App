efe185b276207aa9158127bc9b364733921e3cdf
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-23 03:10:25 -0800

    added cookies and logout function

--- frontend/src/StickyFooter.js
+++ frontend/src/StickyFooter.js
@@ -1,10 +1,21 @@
 import { useNavigate } from 'react-router-dom'
 import { Container, Navbar, Nav, Button, Form, InputGroup } from 'react-bootstrap'
 import { BsSearch, BsStarFill, BsPlus, BsEgg, BsEggFill } from 'react-icons/bs'
+import Cookies from 'universal-cookie'
 
 // TODO: Make nav horizontally aligned and positioned on the bottom
 function StickyFooter() {
     const navigate = useNavigate()
+
+    const cookies = new Cookies()
+
+    const token = cookies.get('TOKEN')
+
+    const logout = () => {
+        // destroy cookie
+        cookies.remove('TOKEN', { path: '/' })
+        navigate('/')
+    }
 
     return (
         <Navbar collapseOnSelect expand='lg' bg='light' fixed='bottom' fill='true'>
@@ -40,7 +51,7 @@ function StickyFooter() {
                         </Nav.Item>
                         {/* TODO: Update Navigate Path For Logout Button */}
                         <Nav.Item className='d-flex p-1'>
-                            <Button variant='danger' size='sm' onClick={() => navigate('/')}>
+                            <Button variant='danger' size='sm' type='submit' onClick={() => logout()}>
                                 <BsEggFill className='mb-1' /> Logout
                             </Button>
                         </Nav.Item>
