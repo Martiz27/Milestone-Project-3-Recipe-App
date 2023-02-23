38675fb1258135c8c52283d3290ea5921314e568
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-23 07:59:01 -0800

    updated CurrentUser Provider with fetch authorization and fixed children to props.children in the return

--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -1,13 +1,32 @@
-import { createContext, useState } from 'react';
+import { useEffect, createContext, useState } from 'react';
+
+import Cookies from 'universal-cookie'
 
 export const CurrentUser = createContext()
 
-function CurrentUserProvider(children) {
+function CurrentUserProvider(props) {
+
+    const cookies = new Cookies()
+
     const [currentUser, setCurrentUser] = useState(null)
+
+    useEffect(() => {
+        const getLoggedInUser = async () => {
+            let response = await fetch(`http://localhost:5000/auth/profile`, {
+                headers: {
+                    'Authorization': `Bearer ${cookies.get('TOKEN')}`
+                }
+            })
+            let user = await response.json()
+            setCurrentUser(user)
+        }
+        getLoggedInUser()
+    }, [])
+
 
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
-            {children}
+            <>{props.children}</>
         </CurrentUser.Provider>
     )
 }
