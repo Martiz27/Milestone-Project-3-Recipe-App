32d4cfa400743867fb4ecceb9e2bd66fb176fc47
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-22 19:55:14 -0800

    updated SignUpForm component with form validation and added extra validators and error messages to Form.Groups

--- frontend/src/users/SignUpForm.js
+++ frontend/src/users/SignUpForm.js
@@ -15,8 +15,17 @@ function SignUpForm() {
         password: ''
     })
 
+    const [validated, setValidated] = useState(false)
+
     async function handleSubmit(e) {
         e.preventDefault()
+        const form = e.currentTarget
+
+        if (form.checkValidity() === false) {
+            e.preventDefault()
+            e.stopPropagation()
+        }
+        setValidated(true)
         await fetch(`http://localhost:5000/users`, {
             method: 'POST',
             headers: {
@@ -31,7 +40,7 @@ function SignUpForm() {
         <Container className='my-5 mx-auto pb-5'>
             <h1 className='text-warning'>Sign Up</h1>
             <hr />
-            <Form onSubmit={handleSubmit}>
+            <Form noValidate validated={validated} onSubmit={handleSubmit}>
                 <Form.Label>
                     <h3 className='text-primary'>
                         Personal Information
@@ -58,6 +67,10 @@ function SignUpForm() {
                                     value={user.firstName}
                                     onChange={e => setUser({ ...user, firstName: e.target.value })}
                                 />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your first name
+                                </Form.Control.Feedback>
                             </FloatingLabel>
                         </Form.Group>
                     </Col>
@@ -75,6 +88,10 @@ function SignUpForm() {
                                     value={user.lastName}
                                     onChange={e => setUser({ ...user, lastName: e.target.value })}
                                 />
+                                <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                                <Form.Control.Feedback type="invalid">
+                                    Please enter your last name
+                                </Form.Control.Feedback>
                             </FloatingLabel>
                         </Form.Group>
                     </Col>
@@ -84,13 +101,18 @@ function SignUpForm() {
                             label='Email' >
                             <Form.Control
                                 required
-                                type='text'
+                                type='email'
+                                minLength='6'
+                                maxLength='127'
                                 placeholder='Email'
                                 id='email'
                                 name='email'
                                 value={user.email}
                                 onChange={e => setUser({ ...user, email: e.target.value })}
                             />
+                            <Form.Control.Feedback type="invalid">
+                                Please provide a valid email address with the following format: name@example.com
+                            </Form.Control.Feedback>
                         </FloatingLabel>
                     </Form.Group>
                 </Row>
@@ -113,12 +135,20 @@ function SignUpForm() {
                             <Form.Control
                                 required
                                 type='text'
+                                minLength='6'
+                                maxLength='32'
                                 placeholder='Username'
                                 id='username'
                                 name='username'
                                 value={user.username}
                                 onChange={e => setUser({ ...user, username: e.target.value })}
                             />
+                            <Form.Text id="usernameHelp" muted>
+                                Your username can be between 6 and 32 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji
+                            </Form.Text>
+                            <Form.Control.Feedback type="invalid">
+                                Please choose a username
+                            </Form.Control.Feedback>
                         </FloatingLabel>
                     </Form.Group>
                     <Col md={12} lg={6}>
@@ -128,16 +158,45 @@ function SignUpForm() {
                                 label='Password'>
                                 <Form.Control
                                     required
-                                    type='text'
+                                    type='password'
+                                    minLength='6'
+                                    maxLength='127'
                                     placeholder='Password'
                                     id='password'
                                     name='password'
                                     value={user.password}
                                     onChange={e => setUser({ ...user, password: e.target.value })}
                                 />
+                                <Form.Control.Feedback type="invalid">
+                                    Please choose a password
+                                </Form.Control.Feedback>
                             </FloatingLabel>
                         </Form.Group>
                     </Col>
+                    <Col md={12} lg={6}>
+                        <Form.Group>
+                            <FloatingLabel
+                                id='floatingPassword'
+                                label='Confirm Password'>
+                                <Form.Control
+                                    required
+                                    type='password'
+                                    minLength='6'
+                                    maxLength='127'
+                                    placeholder='Confirm Password'
+                                    id='confirmPassword'
+                                    name='confirmPassword'
+                                    pattern={user.password}
+                                />
+                                <Form.Control.Feedback type="invalid">
+                                    Passwords must match
+                                </Form.Control.Feedback>
+                            </FloatingLabel>
+                        </Form.Group>
+                    </Col>
+                    <Form.Text id="passwordHelp" muted>
+                        Your password must be a minimum of 6 characters long, contain letters and numbers, and must not contain spaces, special characters, or emoji
+                    </Form.Text>
                 </Row>
                 <ButtonToolbar className='d-flex justify-content-end gap-3'>
                     <Button variant='light' size='sm' onClick={() => navigate('/users/login')}>
@@ -146,9 +205,9 @@ function SignUpForm() {
                     <Button variant='danger' size='sm' onClick={() => navigate('/')}>
                         <BsEggFill className='mb-1' /> Cancel
                     </Button>
-                        <Button variant='warning' size='sm' type='submit' >
-                            <BsEgg className='mb-1' /> Sign Up
-                        </Button>
+                    <Button variant='warning' size='sm' type='submit' >
+                        <BsEgg className='mb-1' /> Sign Up
+                    </Button>
                 </ButtonToolbar>
             </Form>
         </Container>
