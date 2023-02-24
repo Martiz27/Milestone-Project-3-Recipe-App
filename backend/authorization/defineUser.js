a53fff9a8f324894e3d3ecd319ced7f314851003
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2023-02-23 17:18:29 -0800

    added testing for definingCurrentUser authorization

--- backend/authorization/defineUser.js
+++ backend/authorization/defineUser.js
@@ -6,17 +6,16 @@ const { User } = db
 
 async function defineCurrentUser(req, res, next) {
     try {
-        const [method, token] = await req.headers.authorization.split(" ") //[1]
-
+        console.log(`headers: ${req.json()}`)
+        const [method, token] = await req.headers.authorization.split(" ")
+        console.log(`Method: ${method}, Token: ${token}`)
         if (method == 'Bearer') {
-            const decodedToken = await jwt.verify(
-                token,
-                'RANDOM-TOKEN'
-            )
+            const decodedToken = await jwt.verify( token, 'RANDOM-TOKEN')
             const { id } = decodedToken.value
             let user = await User.findOne({ userId: id })
-
             req.currentUser = user
+            console.log(`check define user: ${user}`)
+            console.log(`defining user: ${req.currentUser}`)
         }
         next()
 
@@ -28,6 +27,34 @@ async function defineCurrentUser(req, res, next) {
         req.currentUser = null
         next()
     }
+    
+    // try {
+    //     // console.log(`req: ${JSON.stringify(req)}`)
+    //     // console.log(`req.body: ${JSON.stringify(req.body)}`)
+    //     // console.log(`req.headers: ${JSON.stringify(req.headers)}`)
+    //     // res.send(req)
+    //     console.log(`Auth: req: ${req.json()}`)
+
+    //     console.log(`Auth: defining user headers auth: ${req.headers.authorization}`) // undefined
+    //     const token = await req.headers.authorization.split(' ')[1]
+
+    //     console.log(`Auth: reading token: ${token}`)
+    //     const decodedToken = await jwy.verify(token, "RANDOM-TOKEN")
+
+    //     console.log(`Auth: decoded token ${decodedToken}`)
+    //     const user = await decodedToken
+
+    //     console.log(`Auth: defining user: ${user}`)
+    //     req.user = user
+
+    //     console.log(`Auth: defining current user: ${req.currentUser}`)
+    //     res.json(user)
+    //     next()
+    // } catch (error) {
+    //     // console.log(`defining current user error: ${error}`)
+    //     req.currentUser = null
+    //     next()
+    // }
 }
 
 module.exports = defineCurrentUser
